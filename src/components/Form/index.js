61f0170754aec2c14d8fb3848c97a4e489f9e04b
Ww180516 <101150775+Ww180516@users.noreply.github.com>
2022-05-05 03:03:12 -0300

    segundo

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,28 +1,31 @@
 import React from "react"
-import { Text, TextInput, View, Button } from "react-native"
+import { Text, TextInput, View, Button } from 'react-native'
 import styles from "./style"
 
 export default function Form () {
 
     return (
         <View style={styles.formContext}>
-            <View>
-                <Text>Exercico</Text>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Exercico</Text>
                 <TextInput
+                style={styles.input}
                 placeholder="Ex:20:00"
                 keyboardType="numeric"
                 />
-                <Text>Descanso</Text>
+                <Text style={styles.formLabel}>Descanso</Text>
                 <TextInput
+                style={styles.input}
                 placeholder="Ex:20:00"
                 keyboardType="numeric"
                 />
-                <Text>Repetições</Text>
+                <Text style={styles.formLabel}>Repetições</Text>
                 <TextInput
+                style={styles.input}
                 placeholder="Ex:12"
                 keyboardType="numeric"
                 />
-                <Button title="Começar" />
+                <Button title="Começar"/>
             </View>
         </View>
     );
